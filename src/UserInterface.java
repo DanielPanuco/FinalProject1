17d8229836b8e63e9ccb7fce1365c43707a51057
DanielPanuco <marioapanuco21@gmail.com>
2021-06-07 10:57:44 -0700

    changed 1 line

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -2,6 +2,6 @@ public class UserInterface {
     public static void main(String[] args) {
         final int userSize =100;
         //HashTable<Customer> customers = new HashTable<>(userSize * 2);
-
+        String email;
     }
 }
