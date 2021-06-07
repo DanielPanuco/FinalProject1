a7d9a6d36cd5c0e4010857044c1563d1c2c6ca62
DanielPanuco <marioapanuco21@gmail.com>
2021-06-07 10:53:31 -0700

    Test

---
+++ src/UserInterface.java
@@ -0,0 +1,7 @@
+public class UserInterface {
+    public static void main(String[] args) {
+        final int userSize =100;
+        //HashTable<Customer> customers = new HashTable<>(userSize * 2);
+
+    }
+}
---
+++ src/VideoGame.java
@@ -0,0 +1,16 @@
+public class VideoGame {
+    private String title;
+    private String developer;
+    private int releaseDate;
+    private int metaCriticScore;
+    private String contentRating;
+    private String genre;
+
+    VideoGame(String title, String developer, int releaseDate, int metaCriticScore, String contentRating) {
+
+    }
+
+    public int hashCode() {
+        return 0;
+    }
+}
