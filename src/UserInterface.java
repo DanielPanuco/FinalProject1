af1f824140e2b24ac930549598184860d68b8d16
DanielPanuco <73054034+DanielPanuco@users.noreply.github.com>
2021-06-07 13:21:21 -0700

    Fixed UserInterface switch

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -6,35 +6,7 @@ public class UserInterface {
         // read in files to hashtables
         // sign in
         // call a method for either employee or customer
-        int test;
-        String pull;
-        String test1;
-        while (!choice.equalsIgnoreCase("X")) {
-			printMenu();
-			System.out.print("Enter your choice: ");
-			choice = input.nextLine();
-			switch (choice.toUpperCase()) {
-				case "A":
-					addFund(funds, currentC, input);
-					break;
-				case "B":
-					sellFund(currentC, input);
-					break;
-				case "C":
-					addCash(currentC, input);
-					break;
-				case "D":
-					displayFunds(currentC, input);
-					break;
-				case "X":
-					System.out.println("\nGoodbye!");
-					break;
-				default:
-					System.out.println("\nInvalid menu option."
-							+ " Please enter A-D or X to exit.");
-					break;
-			}
-		}
+
     }
 
     // method here for customer
