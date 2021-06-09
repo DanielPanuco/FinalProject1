2639ccf6aaa92f5614dc434ecd0eb84a1a294cfb
baiwe <baiwe@Alienware-M15>
2021-06-09 13:55:26 -0700

    Updated Employee

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,44 @@
 public class Employee extends User {
-
+	
+	public Employee(){
+		super();
+	}
+	
+	public Employee(String firstName, String lastName, 
+			String email, String password) {
+		super(firstName, lastName, email, password);
+	}
+	
+	public static Customer searchCustomer(String name, HashTable<Customer> customers) {
+		return null;
+	}
+	
+	public static void displayCustomer(Customer customer) {
+		
+	}
+	
+	public static void viewOrders(Heap<Order> orders) {
+		
+	}
+	
+	public static void shipOrder(Heap<Order> orders, Order order) {
+		//orders.remove(order);
+	}
+	
+	public static void listByTitle(BST<VideoGame> byTitle) {
+		
+	}
+	
+	public static void listByDate(BST<VideoGame> byDate) {
+		
+	}
+	
+	public static void addProduct(BST<VideoGame> byTitle, BST<VideoGame> byDate, VideoGame videogame) {
+		
+	}
+	
+	public static void removeProduct(BST<VideoGame> byTitle, BST<VideoGame> byDate, VideoGame videogame) {
+		
+	}
+	
 }
